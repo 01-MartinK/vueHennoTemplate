1d48ae64b70a0aa76aaa6236b7288c9c583abac1
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2022-12-09 12:48:21 +0200

    Express and Websocket simple project

---
+++ public/app.js
@@ -0,0 +1,54 @@
+// main vue script don't really need other scripts but for organisation u may need
+
+const vue = Vue.createApp({
+    data() { // Data or variables in a vue app
+        return {
+            templateList: [],
+            index: 0,
+            admin: false,
+            socket: null,
+            ip: ""
+        }
+    },
+    async created() { // when the app is created/initialized
+        this.socket = io.connect("http://localhost:3010"); // connect to a socket
+        
+        this.socket.on('connect', () => { // on socket connect
+            console.log("connected to server")
+        });
+
+        // on created get the list right away
+        try {
+            this.templateList = await (await fetch('http://localhost:3010/templateList')).json();
+            this.saveToLocalStorage()
+        } catch(err){
+            console.log(err)
+        }
+        
+    },
+    socket: {
+        connect: () => {console.log("connected")}
+    },
+    methods: { // methods should be here | methods that are here sould always be async
+        getTemplateLists: async function() { // get the templateLists
+            const requestOptions = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+
+            await fetch("http://localhost:3010/templateList", requestOptions)
+            .then(response => response.json()) // turn response into json
+            .then(data => { // do what you want
+                this.templateList = data;
+            })
+        },
+        getListById: async function(id) { // get a single list via ID
+
+        },
+        saveToLocalStorage: async function() { // save to local storage the templateList
+            localStorage.setItem('TemplateLists', JSON.stringify(this.templateList));
+        },
+    }
+}).mount('#app');
